8fb3037604e439cceb41f4ca7cbf0384119def4b
jedrulcia <jedrzej.koriat@gmail.com>
2023-04-03 17:02:21 +0200

    first commit

---
+++ Palindrome/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type any text");
+            isPalindrome(Console.ReadLine());
+        }
+        static void isPalindrome(string text)
+        {
+            int n = 0;
+            for (int index = 0, backIndex = text.Length - 1; index < text.Length; index++, backIndex--)
+            {
+                if (text[index] == text[backIndex]) n++;
+            }
+            if (n == text.Length) Console.WriteLine("This text is a palindrome");
+            else Console.WriteLine("This text is not a palindrome");
+        }
+    }
+}
